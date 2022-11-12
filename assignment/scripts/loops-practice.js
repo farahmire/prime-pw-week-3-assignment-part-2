500a68917ec6ca91a8e4d883c78dabc5ddb8f7f5
Farah Mire <farah_mire@yahoo.com>
2022-11-11 20:49:20 -0500

    all questions but end

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -14,16 +14,41 @@ for (let i=0; i<4; i++) {
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
 
+for (let i=0; i<6; i++) {  
+  console.log(i);
+}
+
+// You chsnge the number to one higher than 5, when to stop looping ( stopping condition)
+
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
+
+
+for (let i=3; i<6; i++) {  
+  console.log(i);
+}
+
+// You change I's intializing value to starting number 
+//(3) & then when to stop looping ( stopping condition)
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 
+for (let i=2; i<11; i +=2) {  
+  console.log(i);
+} 
+// You change I's intializing value to starting number (2) & then change when to stop looping 
+// (11) the stopping condition), & lastly th
+// Iteration statement to add (2) each time the loop is finished
+
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
+
+for (let i=5; i>-1; i --) {  
+  console.log(i);
+} 
 
 
 // 2. For of loops  
@@ -34,11 +59,26 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 console.log('Some stars:');
 
 
+for (let i=0; i < stars.length; i++) { 
+  console.log(stars[i]);
+ }
+
+
+
 // 3. While loops  
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
+
+let i = 0;
+
+while (i<stars.length){
+console.log(stars[i])
+i++;
+}
+
+
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
